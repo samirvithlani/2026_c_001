1f34a256b40126afa70950465d6afa9c72cd8159
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-19 14:58:09 +0530

    nested if else

--- iflese/nestedIf2.c
+++ iflese/nestedIf2.c
@@ -7,7 +7,7 @@ void main(){
         printf("\n enter age :");
         scanf("%d",&age);
 
-        //19
+        //15
         if(age>=18){
             printf("\n eligible for DL:");
 
@@ -16,15 +16,19 @@ void main(){
             }
             else{
                 printf("\n not eligible for marrige::");
+                printf("\n years needed for eligible for marrige = %d",21-age);
             }
         }
         else{
             printf("\n NOt eligible for DL");
+            printf("\n years needed for eligible for DL = %d",18-age);
+            
             if(age>=16){
                 printf("\n eligible for L DL");
             }
             else{
                 printf("\n stay at home:");
+                printf("\n years needed for eligible for l DL = %d",16-age);
             }
 
         }
