cf858e580d8a96196b7d1f47abc48afa2748b291
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-22 14:32:32 +0530

    logica ap

---
+++ iflese/logicalOpDemo2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    
+    //1 -- true, 0 false
+    int isPancard = 0,isAdhar = 0;
+    if(isAdhar == 1 || isPancard == 1){
+        printf("\n valid application..");
+    }
+    else{
+        printf("\n not valid application !!");
+    }
+    
+}
---
+++ iflese/logocalOpDemo1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+    //even -->20>
+    int no=26;
+    if(no%2==0 && no>20){
+        printf("\n valid...");
+    }
+    else{
+        printf("\n no is not valid !!");
+    }
+   
+}
