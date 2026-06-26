ec376544f2975155043554273b7783e49a1a7609
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-26 14:57:00 +0530

    swith choice

--- switchcase/switchDemo1.c
+++ switchcase/switchDemo1.c
@@ -3,33 +3,46 @@
 void main()
 {
 
-    int choice;
+    int choice,no1,no2,ans;
     printf("Press 1 for ADD :");
     printf("\nPress 2 for MUL :");
     printf("\nPress 3 for DIV :");
     printf("\nPress 4 for SUB :");
-
     scanf("%d",&choice); //1
+
+    printf("\n enter no 1:");
+    scanf("%d",&no1);
+    printf("\n enter no 2:");
+    scanf("%d",&no2);
 
     switch(choice){
 
         case 1:
-            printf("\n case 1 add !!");
+            printf("\n ADD of 2 numbers!!");
+            ans = no1 + no2;
+            //printf("\n ans = %d",ans);
         break;
         case 2:
-            printf("\n case 2 mul!!");
+            printf("\n MUL of 2 numbers!!");
+            ans = no1 * no2;
+            //printf("\n ans = %d",ans);
         break;
         case 3:
-            printf("\n div called...");
+            printf("\n DIV of 2 numbers!!");
+            ans = no1 / no2;
+            //printf("\n ans = %d",ans);
         break;    
         case 4:
-            printf("\n sub called.. !!");
+            printf("\n SUB of 2 numbers!!");
+            ans = no1 - no2;
+            //printf("\n ans = %d",ans);
         break;
         default:
             printf("\n invalid choice !!!");
         break;
 
-
     }
+
+    printf("\n ans = %d",ans);
 
 }
