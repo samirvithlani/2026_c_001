da8cad588894eefb246ab5619c658a20214d4d75
samirvithlani <samir.vithlani83955@gmail.com>
2026-07-08 14:41:03 +0530

    palindrome

--- loops/whileDemo5.c
+++ loops/whileDemo5.c
@@ -3,14 +3,21 @@
 void main()
 {
     
-    int no=123;
-    int sum=0,rem;
+    int no=12;
+    int sum=0,rem,mul=1;
     while(no!=0){
         rem = no % 10;
         sum = sum + rem;
+        mul = mul * rem;
         no = no / 10;
     }
 
     printf("\n sum =%d",sum);
+    if(sum==mul){
+        printf("\n this no is twin no:");
+    }
+    else{
+        printf("\n this no is not twin no:");
+    }
    
 }
