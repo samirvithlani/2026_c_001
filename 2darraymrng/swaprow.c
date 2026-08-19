d948689923a8c930fd950f65a6689a9f65da8bce
samirvithlani <samir.vithlani83955@gmail.com>
2026-08-19 09:47:22 +0530

    swap row morning

---
+++ 2darraymrng/swaprow.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+{
+    
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}},i,j,temp;
+    for(i=0;i<3;i++){
+        //j=0,j=1
+        for(j=0;j<3;j++){
+                temp = a[i][j]; //temp=1 , temp =2
+                a[i][j]=a[2][j];//a[0][0]=7 ,a[0][1]=8
+                a[2][j] = temp; //a[2][0]=1 ,a[2][j]=2s
+        }
+    }//swap loop over
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf(" %d ",a[i][j]);
+        }
+        printf("\n");
+    }
+   
+}
