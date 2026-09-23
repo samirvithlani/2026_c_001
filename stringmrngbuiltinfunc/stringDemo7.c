8bf4c0baefae0cb0083c97f486172b1feb373def
samirvithlani <samir.vithlani83955@gmail.com>
2026-09-23 09:15:33 +0530

    string

---
+++ stringmrngbuiltinfunc/stringDemo7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    
+    char str1[100]="Hello ",str2[100]="world";
+    strcat(str1,str2);
+    printf("\n str1 = %s",str1);
+    printf("\n str2 = %s",str2);
+   
+}
